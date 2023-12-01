428c9bddcf0a78d8bd787f1f8fa08d090b268de7
Jianhao Feng <jianhao.feng@ucdconnect.ie>
2023-12-01 11:20:09 +0000

    add toggles

--- app/src/pages/Result.js
+++ app/src/pages/Result.js
@@ -2,11 +2,21 @@ import React, {useEffect, useState} from "react";
 import { useNavigate } from 'react-router-dom';
 import '../styles/common.css';
 import Search from "../components/Search.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquarePlus } from '@fortawesome/free-regular-svg-icons';
 
 
 const Result = () => {
     const [returned, setReturned] = useState({})
-    const navigate = useNavigate();
+    const [collapsed, setCollapsed] = useState({});
+
+    const toggleCollapse = (key) => {
+        console.log(key)
+        setCollapsed(prevStates => ({
+            ...prevStates,
+            [key]: !prevStates[key]
+        }));
+    };
     
     useEffect(() => {  
         
@@ -23,31 +33,49 @@ const Result = () => {
             }
         })
         .then(text => {
-            console.log("here3");
+            // console.log("here3");
             setReturned(text);
         })
         .catch(error => {
-            console.log("here4");
+            // console.log("here4");
             console.error('Error:', error);
         });
 
-    }, [returned])
+    }, [])
+
+    useEffect(() => {
+        if (Object.keys(returned).length > 0) {
+            const newCollapsedStates = {};
+            Object.keys(returned).forEach(key => {
+                if (typeof returned[key] === 'object' && (Object.keys(returned[key]).length > 3 || returned[key].length > 3)) {
+                    console.log(key, returned[key], returned[key].length > 3)
+                    newCollapsedStates[key] = true; 
+                }
+            });
+            setCollapsed(newCollapsedStates);
+        }
+    }, [returned]); 
 
     const unpack = (obj) => {
         if(Object.keys(obj).length > 0){
-            const keys = Object.keys(obj)
-            // const items = Object.entries(obj)
+            const keys = Object.keys(obj);
+         
             return(
                 <ul>
-                    {keys.map((key) => (
-                        
-                        obj[key] !== null && Array.isArray(obj[key]) 
-                        
-                            ? <ul><li key={key.id}>{key}: {obj[key]}</li></ul>
+                    {keys.map((key) => (                  
+                        obj[key] !== null && Array.isArray(obj[key])                    
+                            ? ( obj[key].length > 3 
+                                ?<ul><li><span onClick={ () => toggleCollapse(key)}>{key}: {collapsed[key] ?  <FontAwesomeIcon icon={faSquarePlus} beatFade /> : obj[key].join(', ')}</span></li></ul>
+                                :<ul><li>{key}: {obj[key].join(', ')} </li></ul>
+                            )
                             :(
                                 typeof obj[key] !== 'object' 
-                                ? <ul><li key={key.id}>{key}: {obj[key].toString()}</li></ul>
-                                : <ul><li key={key.id}>{key}: {unpack(obj[key])}</li></ul>
+                                ? <ul><li>{key}: {obj[key].toString()}</li></ul>
+                                : ( Object.keys(obj[key]).length > 3
+                                    ? <ul><li><span onClick={ () => toggleCollapse(key)}>{key}: {collapsed[key] ? <FontAwesomeIcon icon={faSquarePlus} beatFade/> : unpack(obj[key]) }</span></li></ul>
+                                    : <ul><li>{key}: {unpack(obj[key])}</li></ul>
+                                )
+                               
                             )
                       
 
@@ -65,7 +93,7 @@ const Result = () => {
         <>
             <div className="fixed-container">
                 <Search />
-                { returned.name && <h2>Information for: {returned.name.common} / { returned.name.official} </h2>}
+                { returned.name && <h2>Information for: {returned.name.common} / { returned.name.official} { returned.flag}</h2>}
                 
                 
             </div>
